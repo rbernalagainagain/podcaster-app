57d26d8b81357a54a21595f3aea4389568b84920
Rafa bernal <rbernalber@pm.me>
2024-04-30 13:39:33 +0200

    fix: base router and config to producction

--- src/core/router/router.tsx
+++ src/core/router/router.tsx
@@ -2,30 +2,35 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from '../../App.tsx'
 import { Podcasts } from '../../pages/podcasts/Podcasts.tsx'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/',
+          element: <Podcasts />,
+        },
+        {
+          lazy: () => import('../../shared/layout/detail/LayoutDetail'),
+          children: [
+            {
+              path: '/podcast/:podcastId',
+              lazy: () => import('../../pages/podcast-detail/PodcastDetail'),
+            },
+            {
+              path: '/podcast/:podcastId/episode/:episodeId',
+              lazy: () => import('../../pages/episode/Episode'),
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Podcasts />,
-      },
-      {
-        lazy: () => import('../../shared/layout/detail/LayoutDetail'),
-        children: [
-          {
-            path: '/podcast/:podcastId',
-            lazy: () => import('../../pages/podcast-detail/PodcastDetail'),
-          },
-          {
-            path: '/podcast/:podcastId/episode/:episodeId',
-            lazy: () => import('../../pages/episode/Episode'),
-          },
-        ],
-      },
-    ],
+    basename: process.env.NODE_ENV === 'production' ? '/podcaster-app/' : '/',
   },
-])
+)
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   outDir: 'build',
-  // },
-  base: process.env.NODE_ENV === 'production' ? 'https://rbernalagainagain.github.io/podcaster-app/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/podcaster-app/' : '/',
   resolve: {
     alias: [
       {

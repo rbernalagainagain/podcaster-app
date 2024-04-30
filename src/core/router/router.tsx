866d1992a909422526771c5a17d14ffb93d942b2
Rafa bernal <rbernalber@pm.me>
2024-04-30 14:32:39 +0200

    chore: clean router to production
    vercel allows same route in production and local

--- src/core/router/router.tsx
+++ src/core/router/router.tsx
@@ -2,35 +2,30 @@ import { createBrowserRouter } from 'react-router-dom'
 import App from '../../App.tsx'
 import { Podcasts } from '../../pages/podcasts/Podcasts.tsx'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-      children: [
-        {
-          path: '/',
-          element: <Podcasts />,
-        },
-        {
-          lazy: () => import('../../shared/layout/detail/LayoutDetail'),
-          children: [
-            {
-              path: '/podcast/:podcastId',
-              lazy: () => import('../../pages/podcast-detail/PodcastDetail'),
-            },
-            {
-              path: '/podcast/:podcastId/episode/:episodeId',
-              lazy: () => import('../../pages/episode/Episode'),
-            },
-          ],
-        },
-      ],
-    },
-  ],
+const router = createBrowserRouter([
   {
-    // basename: process.env.NODE_ENV === 'production' ? '/podcaster-app/' : '/',
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Podcasts />,
+      },
+      {
+        lazy: () => import('../../shared/layout/detail/LayoutDetail'),
+        children: [
+          {
+            path: '/podcast/:podcastId',
+            lazy: () => import('../../pages/podcast-detail/PodcastDetail'),
+          },
+          {
+            path: '/podcast/:podcastId/episode/:episodeId',
+            lazy: () => import('../../pages/episode/Episode'),
+          },
+        ],
+      },
+    ],
   },
-)
+])
 
 export default router
--- src/register-sw.tsx
+++ src/register-sw.tsx
@@ -1,11 +1,8 @@
 export function registerSW() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(
-      import.meta.env.MODE === 'production' ? '/podcaster-app/sw.js' : '/dev-sw.js?dev-sw',
-      {
-        scope: './',
-        updateViaCache: 'none',
-      },
-    )
+    navigator.serviceWorker.register(import.meta.env.MODE === 'production' ? '/sw.js' : '/dev-sw.js?dev-sw', {
+      scope: './',
+      updateViaCache: 'none',
+    })
   }
 }

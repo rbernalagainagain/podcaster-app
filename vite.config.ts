ba842445fd1028cc7e533d7021b7ca3aca37e89c
Rafa bernal <rbernalber@pm.me>
2024-04-30 13:20:07 +0200

    chore: modify gitignore
    upload dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build',
-  },
-  base: 'https://rbernalagainagain.github.io/',
+  // build: {
+  //   outDir: 'build',
+  // },
+  base: 'https://rbernalagainagain.github.io/podcaster-app/',
   resolve: {
     alias: [
       {
